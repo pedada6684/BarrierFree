69586c90924be1e202eed0b4df43bbcc0e435db6
pedada <korea9300@naver.com>
2024-03-07 13:54:51 +0900

    feat: PlaceService.createPlace 추가

--- Backend/src/main/java/com/fullship/hBAF/domain/place/service/PlaceService.java
+++ Backend/src/main/java/com/fullship/hBAF/domain/place/service/PlaceService.java
@@ -2,15 +2,8 @@ package com.fullship.hBAF.domain.place.service;
 
 import com.fullship.hBAF.domain.place.entity.Place;
 import com.fullship.hBAF.domain.place.repository.PlaceRepository;
-import com.fullship.hBAF.global.api.response.TransitPathForm;
-import com.fullship.hBAF.global.api.service.TMapApiService;
-import com.fullship.hBAF.global.api.service.command.SearchPathToTransitCommand;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import com.fullship.hBAF.domain.place.service.command.CreatePlaceCommand;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.parser.ParseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -23,30 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class PlaceService {
 
   private final PlaceRepository placeRepository;
-  private final TMapApiService tMapApiService;
-
-  public String useTransitPath(SearchPathToTransitCommand command) throws ParseException {
-    List<TransitPathForm> list = tMapApiService.searchPathToTransit(command);
-
-    List<TransitPathForm> transferList = new ArrayList<>(list);
-    Collections.sort(transferList,
-        (o1, o2) -> (int) (o1.getTransferCount() - o2.getTransferCount()));
-
-    int idx = 0;
-    long min = transferList.get(0).getTransferCount();
-    for (int i = 0; i < transferList.size(); i++, idx++) {
-      if(transferList.get(i).getTransferCount() > min + 1)
-        break;
-    }
-
-    for(int i = idx; i < transferList.size(); i++){
-      transferList.remove(idx);
-    }
-
-
-
-    return null;
-  }
 
   @Transactional(readOnly = false)
   public Long createPlace(CreatePlaceCommand command){

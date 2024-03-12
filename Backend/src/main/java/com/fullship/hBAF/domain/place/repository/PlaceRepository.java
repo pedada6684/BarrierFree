aef0e401f8d83b42f5f3cfc157c257f9c6345a25
pedada <korea9300@naver.com>
2024-03-12 15:16:46 +0900

    feat: PlaceService.updatePlaceImageUrl 추가

--- Backend/src/main/java/com/fullship/hBAF/domain/place/repository/PlaceRepository.java
+++ Backend/src/main/java/com/fullship/hBAF/domain/place/repository/PlaceRepository.java
@@ -3,9 +3,12 @@ package com.fullship.hBAF.domain.place.repository;
 import com.fullship.hBAF.domain.place.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
     public Place findPlaceByPoiId(String PoiId);
 
     boolean existsByPoiId(String poiId);
+    Optional<Place> findByPoiId(String poiId);
 }
--- Backend/src/main/java/com/fullship/hBAF/domain/place/service/PlaceService.java
+++ Backend/src/main/java/com/fullship/hBAF/domain/place/service/PlaceService.java
@@ -1,13 +1,18 @@
 package com.fullship.hBAF.domain.place.service;
 
+import com.fullship.hBAF.domain.place.entity.Image;
 import com.fullship.hBAF.domain.place.entity.Place;
+import com.fullship.hBAF.domain.place.repository.ImageRepository;
 import com.fullship.hBAF.domain.place.repository.PlaceRepository;
 import com.fullship.hBAF.domain.place.service.command.CreatePlaceCommand;
+import com.fullship.hBAF.domain.place.service.command.UpdatePlaceImageCommand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -16,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class PlaceService {
 
   private final PlaceRepository placeRepository;
+  private final ImageRepository imageRepository;
 
   @Transactional(readOnly = false)
   public Long createPlace(CreatePlaceCommand command){
@@ -38,5 +44,19 @@ public class PlaceService {
     }
     //검색결과를 기반으로 entity를 만들고 save함
     return placeRepository.save(newPlace).getId();
+  }
+
+  @Transactional(readOnly = false)
+  public Long updatePlaceImageUrl(UpdatePlaceImageCommand command){
+    Place place = placeRepository.findByPoiId(command.getPoiId()).orElseThrow(() -> new IllegalStateException());
+    Optional<Image> imageOptional = imageRepository.findByPlaceAndImageType(place, 0);
+    if (imageOptional.isPresent()){
+      Image image = imageOptional.get();
+      image.updateImageUrl(command.getImageUrl());
+    }else { // 썸네일 이미지 없는 생성
+      Image newImage = Image.createNewImage(place, command.getImageUrl(), 0);
+      imageRepository.save(newImage);
+    }
+    return place.getId();
   }
 }

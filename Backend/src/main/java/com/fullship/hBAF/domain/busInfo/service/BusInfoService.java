247f24d061fa2162c6d5c7ac6f8f1c6e9ebe9cde
이민형 <lllgee@naver.com>
2024-03-12 12:58:26 +0900

    feat: BusInfo 생성

---
+++ Backend/src/main/java/com/fullship/hBAF/domain/busInfo/service/BusInfoService.java
@@ -0,0 +1,26 @@
+package com.fullship.hBAF.domain.busInfo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BusInfoService {
+
+}

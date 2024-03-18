020398e0a4702ebc6e828b2af65ad195ef3b3b15
이민형 <lllgee@naver.com>
2024-03-18 22:08:51 +0900

    feat: PlaceRepository findPlaceByPoiId 기능 추가

--- Backend/src/main/java/com/fullship/hBAF/domain/place/repository/PlaceRepository.java
+++ Backend/src/main/java/com/fullship/hBAF/domain/place/repository/PlaceRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
+    public Place findPlaceByPoiId(String PoiId);
+
 }

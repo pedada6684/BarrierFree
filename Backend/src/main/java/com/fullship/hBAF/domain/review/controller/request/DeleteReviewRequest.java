1c856de234d8aa0d77a0185c9fdba2723f7e44cd
이민형 <lllgee@naver.com>
2024-03-19 13:47:43 +0900

    feat: review 삭제 기능 추가

---
+++ Backend/src/main/java/com/fullship/hBAF/domain/review/controller/request/DeleteReviewRequest.java
@@ -0,0 +1,10 @@
+package com.fullship.hBAF.domain.review.controller.request;
+
+import lombok.Getter;
+
+@Getter
+public class DeleteReviewRequest {
+
+    Long reviewId;
+
+}
---
+++ Backend/src/main/java/com/fullship/hBAF/domain/review/service/command/request/DeleteReviewRequestCommand.java
@@ -0,0 +1,12 @@
+package com.fullship.hBAF.domain.review.service.command.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class DeleteReviewRequestCommand {
+
+    Long reviewId;
+
+}

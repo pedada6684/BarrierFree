3f4f33cd4adc4a563b5850a98e6f2bd3ca626ae6
김은지 <eungg0214@naver.com>
2024-03-19 15:43:35 +0900

    docs: key file 추가

--- barrier_free/lib/screen/map/map_screen.dart
+++ barrier_free/lib/screen/map/map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:barrier_free/component/appBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -11,6 +12,44 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   static const platform = MethodChannel('com.example.barrier_free/tmap');
+
+  Future<void> _enableTmapTrackingMode() async {
+    try {
+      final String result = await platform.invokeMethod('enableTrackingMode');
+      print('TMap 추적모드 활성화 : $result');
+    } on PlatformException catch (e) {
+      print("내 위치 추적 실패:'${e.message}'");
+    }
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('위치 서비스가 활성화 되어 있지 않습니다.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      //거절일경우
+      permission = await Geolocator.requestPermission();
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('위치 권한을 설정에서 허가해주세요,');
+    }
+
+    final position = Geolocator.getCurrentPosition();
+    await _enableTmapTrackingMode();
+    return position;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,15 +67,5 @@ class _MapScreenState extends State<MapScreen> {
         creationParamsCodec: StandardMessageCodec(),
       ),
     );
-  }
-
-  Future<void> _showTMapView() async {
-    try {
-      final String result = await platform.invokeMethod('showTMap');
-
-      print('결과 : $result');
-    } on PlatformException catch (e) {
-      print("지도 불러오기 실패! '${e.message}'");
-    }
   }
 }

03a317d152a89d8aa989930be86d19aa234071ca
이태경 <korea9300@naver.com>
2024-03-20 17:58:19 +0900

    feat: OAuthLoginService 추가

--- Backend/src/main/java/com/fullship/hBAF/domain/member/entity/Member.java
+++ Backend/src/main/java/com/fullship/hBAF/domain/member/entity/Member.java
@@ -3,33 +3,33 @@ package com.fullship.hBAF.domain.member.entity;
 import com.fullship.hBAF.domain.bookmarkPlace.entity.BookmarkPlace;
 import com.fullship.hBAF.domain.review.entity.Review;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Getter
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
   @Id
   @GeneratedValue
   @Column(name = "member_id")
   private Long id;
-
+  @Column(unique = true)
   private String email;
-
-  private String name;
-
+  private String password;
   private String nickname;
-
+  private String username;
   private Long role;
-
   private LocalDateTime regDate;
-
   private Long status;
-
   private Long delDate;
+  private OAuthProvider provider;
 
   @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
   private List<BookmarkPlace> bookmarkPlaces;
@@ -37,5 +37,17 @@ public class Member {
   @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
   private List<Review> reviews;
 
-
+  public static Member createNewMember(
+          String email,
+          String password,
+          String nickname,
+          String username
+  ){
+    Member member = new Member();
+    member.email = email;
+    member.password = password;
+    member.nickname = nickname;
+    member.username = username;
+    return member;
+  }
 }

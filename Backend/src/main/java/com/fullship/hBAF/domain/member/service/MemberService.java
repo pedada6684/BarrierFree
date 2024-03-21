4f26fff551a7fecc75dfc8e9ea1133164832e3fe
이태경 <korea9300@naver.com>
2024-03-21 15:34:48 +0900

    feat: MemberService.findUserByEmail 추가

--- Backend/src/main/java/com/fullship/hBAF/domain/member/service/MemberService.java
+++ Backend/src/main/java/com/fullship/hBAF/domain/member/service/MemberService.java
@@ -1,12 +1,24 @@
 package com.fullship.hBAF.domain.member.service;
 
+import com.fullship.hBAF.domain.member.entity.Member;
 import com.fullship.hBAF.domain.member.repository.MemberRepository;
+import com.fullship.hBAF.domain.member.service.command.FindMemberByEmailCommand;
+import com.fullship.hBAF.global.response.ErrorCode;
+import com.fullship.hBAF.global.response.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
   private final MemberRepository memberRepository;
+
+  public Long findUserByEmail(FindMemberByEmailCommand command){
+    Member member = memberRepository.findByEmail(command.getEmail())
+            .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
+    return member.getId();
+  }
 }

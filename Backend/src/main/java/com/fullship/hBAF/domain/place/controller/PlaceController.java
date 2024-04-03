b4bea09fd1419731839f6292e638bdf1974a1e60
신유진 <yujin1698@naver.com>
2024-04-03 21:36:03 +0900

    refactor: PlaceController log 추가

--- Backend/src/main/java/com/fullship/hBAF/domain/place/controller/PlaceController.java
+++ Backend/src/main/java/com/fullship/hBAF/domain/place/controller/PlaceController.java
@@ -48,6 +48,8 @@ public class PlaceController {
   @Operation(summary = "휠체어 도보 경로 조회", description = "계단이 없는 보행자 도보를 이용한 휠체어 이동 경로 조회")
   @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = WheelPathForm.class)))
   public ResponseEntity<CommonResponseEntity> searchPathByWheel(@RequestBody PathSearchToWheelRequest requestDto) {
+    System.out.println("**************************************휠체어 도보 경로 조회**********searchPathByWheel*******************************************************************");
+    log.info("PathSearchToWheelRequest: {}", requestDto);
     SearchPathToWheelCommand command = requestDto.createForWheel();
     WheelPathForm wheelPathForm = placeService.useWheelPath(command);
 
@@ -70,6 +72,8 @@ public class PlaceController {
   @Operation(summary = "대중교통 경로 조회", description = "대중교통을 이용하는 경로 조회")
   @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = OdSayPath.class)))
   public ResponseEntity<CommonResponseEntity> searchPathByTransit(@RequestBody PathSearchToTrafficRequest requestDto) {
+    System.out.println("********************************************대중교통 경로 조회***************searchPathByTransit********************************************************");
+    log.info("PathSearchToTrafficRequest: {}", requestDto);
     OdSayPathCommand command = requestDto.createForSearch();
     return getResponseEntity(SuccessCode.OK, placeService.useTransitPath(command));
   }
@@ -78,6 +82,8 @@ public class PlaceController {
   @Operation(summary = "경사도 측정", description = "경로 내 휠체어 경로 경사도 측정")
   @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = PathGeoCode.class)))
   public ResponseEntity<CommonResponseEntity> calculateAngleSlope(@RequestBody CalculateAngleRequest requestDto){
+    System.out.println("********************************************경사도 측정***************calculateAngleSlope********************************************************");
+    log.info("CalculateAngleRequest = {}", requestDto);
     log.info("PathGeoCodes = {}", requestDto.getPathGeoCodes());
     AngleSlopeCommand command = requestDto.createForCalculate();
     return getResponseEntity(SuccessCode.OK, placeService.calculateAngle(command));
@@ -87,6 +93,8 @@ public class PlaceController {
   @Operation(summary = "택시 경로 조회", description = "택시를 이용하는 경로 조회")
   @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = TaxiPathForm.class)))
   public ResponseEntity<CommonResponseEntity> searchPathByTaxi(@RequestBody PathSearchToTrafficRequest requestDto) {
+    System.out.println("********************************************택시 경로 조회***************searchPathByTaxi********************************************************");
+    log.info("PathSearchToTrafficRequest = {}", requestDto);
     SearchPathToTrafficCommand command = requestDto.createForTaxi();
     return getResponseEntity(SuccessCode.OK, placeService.useTaxiPath(command));
   }
@@ -94,14 +102,18 @@ public class PlaceController {
   @PostMapping("/list")
   @Operation(summary = "장애 편의 시설 목록 불러오기", description = "장애 편의 시설 목록 불러오기")
   @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = PlaceListResponse.class)))
-  public ResponseEntity<CommonResponseEntity> getPlaceList(@RequestBody GetPlaceListRequest request) {
-    List<PlaceListResponse> placeList = placeService.getPlaceList(request.toCommand());
+  public ResponseEntity<CommonResponseEntity> getPlaceList(@RequestBody GetPlaceListRequest requestDto) {
+    System.out.println("********************************************장애 편의 시설 목록 불러오기***************getPlaceList********************************************************");
+    log.info("GetPlaceListRequest = {}", requestDto);
+    List<PlaceListResponse> placeList = placeService.getPlaceList(requestDto.toCommand());
     return getResponseEntity(SuccessCode.OK, placeList);
   }
 
   @GetMapping
   @Operation(summary = "장애 편의 시설 불러오기", description = "장애 편의 시설 불러오기")
   public ResponseEntity<GetPlaceResponse> getPlace(@RequestParam("poiId") String poiId){
+    System.out.println("********************************************장애 편의 시설 불러오기***************getPlace********************************************************");
+    log.info("String = {}", poiId);
     GetPlaceResponse response = placeService.getPlace(poiId);
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
@@ -110,6 +122,6 @@ public class PlaceController {
   @Operation(summary = "통신 테스트", description = "통신 테스트")
   public ResponseEntity<CommonResponseEntity> test() throws ParseException {
     System.out.println("통신 테스트");
-    return getResponseEntity(SuccessCode.OK, "[0402-0718]테스트입니다.");
+    return getResponseEntity(SuccessCode.OK, "[0403-0933]테스트입니다.");
   }
 }
